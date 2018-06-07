72a43604e679755b8adfc371f9408f3539b68ed1
Lee <dlehdrl9638@gmail.com>
2018-06-07 20:27:17 +0900

    change program work way : loop -> crontab

--- ProcessInformation/python/deleteOldData/delete_old_data/getMongodbAndDelete.py
+++ ProcessInformation/python/deleteOldData/delete_old_data/getMongodbAndDelete.py
@@ -1,11 +1,12 @@
 '''
 작성자 : DongKi Lee
 기록 시작일 : 2018-06-02
-기능 : 
+기능 :
 일정 시간마다 돌면서, 하루에 한번씩 계산된 데이터를 초기화 해주는 모듈
-change log : 
+change log :
 2018-06-02 : 설명 추가
 2018-06-02 : 중복되는 부분 수정
+2018-06-07 : delete loop, enroll in crontab this program.
 '''
 
 from datetime import datetime
@@ -15,26 +16,19 @@ import time
 MONGODB_PORT = 26543
 MONGODB_IP = "localhost"
 
-while (True):
-    try:
-        now = datetime.now()
-        if now.hour == 4:
+try:
+    client = pymongo.MongoClient(MONGODB_IP, MONGODB_PORT)
 
-            client = pymongo.MongoClient(MONGODB_IP, MONGODB_PORT)
+    database_name = "twitter_api"
+    database = client[database_name]
+    email_db = client["email_list"]
 
-            database_name = "twitter_api"
-            database = client[database_name]
-            email_db = client["email_list"]
+    collection = database.collection_names(include_system_collections=False)
+    for collect in collection:
+        if "mapReduce" in collect:
+            database.drop_collection(collect)
+    email_db.drop_collection("email_list")
+    print("delete mapreduce data and wait 2 hours")
 
-            collection = database.collection_names(include_system_collections=False)
-            for collect in collection:
-                if "mapReduce" in collect:
-                    database.drop_collection(collect)
-            email_db.drop_collection("email_list")
-            print("delete mapreduce data and wait 2 hours")
-
-    except Exception as e:
-        print(e)
-
-    print("wait 35")
-    time.sleep(2100)
+except Exception as e:
+    print(e)
