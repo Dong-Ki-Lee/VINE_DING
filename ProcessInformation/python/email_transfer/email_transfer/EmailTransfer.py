a3236c250d6ce175ea6197499e509d8734c41136
Lee <dlehdrl9638@gmail.com>
2018-06-07 20:29:00 +0900

    change how email information is retrieved

--- ProcessInformation/python/email_transfer/email_transfer/EmailTransfer.py
+++ ProcessInformation/python/email_transfer/email_transfer/EmailTransfer.py
@@ -1,12 +1,12 @@
 '''
 작성자 : DongKi Lee
 기록 시작일 : 2018-06-02
-기능 : 
+기능 :
 계산 요청을 한 고객들의 요청이 완료 되었을 경우,
 해당 고객들에게 이메일을 자동으로 보내주는 모듈
-change log : 
+change log :
 2018-06-02 : 설명 추가
-2018-06-02 : 중복되는 부분 
+2018-06-02 : 중복되는 부분
 '''
 
 import smtplib
@@ -14,13 +14,18 @@ import pymongo
 import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import json
 
-MAIL_ACCOUNT = "your gmail account id"
-MAIL_PASSWORD = "your gmail account pw"
+with open('/home/idong-gi/VINEDING/email_information') as data_file:
+    email_data = json.load(data_file)
+
+MAIL_ACCOUNT = email_data["id"]
+MAIL_PASSWORD = email_data["pw"]
 TITLE = "VINE-DING 에서 당신의 검색어가 준비 되었음을 알려드립니다."
 CONTENT = "저희 VINE-DING 을 이용해 주셔서 감사합니다. 다음 링크를 클릭하여 당신의 검색어를 확인해 주세요 http://vineding.tk/emotion?search_word="
 MONGODB_PORT = 26543
 MONGODB_IP = "localhost"
+
 
 
 def sending_email_using_gmail(to, title, description):
